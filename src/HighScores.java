a21ba61c4d51f0fa667cd83cf262317d3408c4a5
Jesse Rodarte <jerodarte@gmail.com>
2019-02-03 15:12:25 -0800

    HighScores
    assignment 5

---
+++ src/HighScores.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 02/03/2019
+// Jesse E Rodarte
+// CS111B
+// Assignment 5: HighScores
+
+public class HighScores {
+    public static void main(String[] args) {
+        do {
+            ArrayList<String> names = new ArrayList<>();
+            ArrayList<Integer> scores = new ArrayList<>();
+
+            initialize(names, scores);
+            sort(names, scores);
+            display(names, scores);
+        } while (shouldPlayAgain());
+    }
+
+
+
+    public static void initialize(ArrayList<String> names, ArrayList<Integer> scores) {
+        Scanner input = new Scanner(System.in);
+        String tempName;
+        Integer tempScore;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter the name for score #" + (i + 1) + ": ");
+            tempName = input.nextLine();
+            System.out.print("Enter the score for score #" + (i + 1) + ": ");
+            if (input.hasNextInt()) {
+                tempScore = input.nextInt();
+                scores.add(tempScore);
+                names.add(tempName);
+                input.nextLine();
+            } else {
+                System.out.println("Invalid input. Please try again.  ");
+                input.nextLine();
+                System.out.println();
+                i--;
+            }
+        }
+    }
+
+    public static void sort(ArrayList<String> names, ArrayList<Integer> scores) {
+        boolean sorted = false;
+        int passes = 1;
+        int size = names.size();
+        while (!sorted && passes < size)
+        {
+            sorted = true;
+            for (int i = 0; i < (size - 1); i++)
+            {
+                int nextI = i + 1;
+                if (scores.get(i) < scores.get(nextI))
+                {
+                    swap(names, scores, i, nextI);
+                    sorted = false;
+                }
+            }
+            passes++;
+        }
+    }
+
+    public static void display(ArrayList<String> names, ArrayList<Integer> scores) {
+        System.out.println("Top Scorers: ");
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i) + ": " + scores.get(i));
+        }
+    }
+
+    public static void swap(ArrayList<String> names, ArrayList<Integer> scores, int i, int nextI) {
+        String tempName = names.get(i);
+        Integer tempScore = scores.get(i);
+
+        names.set(i, names.get(nextI));
+        scores.set(i, scores.get(nextI));
+
+        names.set(nextI, tempName);
+        scores.set(nextI, tempScore);
+    }
+
+    public static boolean shouldPlayAgain() {
+        char response;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Do you want to try again? (y/n): ");
+        response = input.next().charAt(0);
+        return (response == 'y');
+    }
+}
--- src/PokerHands.java
+++ src/PokerHands.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
+// 02/02/2019
+// Jesse E Rodarte
+// CS111B
+// Assignment 4: PokerHands
+
 public class PokerHands {
+    public static final int HANDSIZE = 5;
+
     public static void main(String[] args) {
         do {
             judgeHand();
@@ -35,18 +43,29 @@ public class PokerHands {
     }
 
     public static int[] getHand() {
-        int[] hand = new int[5];
+        int[] hand = new int[HANDSIZE];
         int card;
-        for (int i = 0; i < 5; i++) {
-            System.out.print("");
+
+        System.out.println("Enter five numeric cards, no face cards. Use 2 - 9 only.");
+        for (int i = 0; i < HANDSIZE; i++) {
+            System.out.print("Card " + (i + 1) + ": ");
             card = getCard();
+            hand[i] = card;
         }
+        Arrays.sort(hand);
         return hand;
     }
 
     public static int getCard() {
-
-        return 2;
+        int card;
+        Scanner input = new Scanner(System.in);
+        card = input.nextInt();
+        while (card < 2 || card > 9) {
+            System.out.println("That's not a valid number!!!");
+            System.out.print("Try again. Use 2 - 9 only: ");
+            card = input.nextInt();
+        }
+        return card;
     }
 
     public static boolean shouldPlayAgain() {
@@ -58,32 +77,80 @@ public class PokerHands {
     }
 
     public static boolean containsFourOfaKind(int[] hand) {
-        boolean result = false;
-        return result;
+        for (int i = 0; i < HANDSIZE; i++) {
+            for (int j = i + 1; j < HANDSIZE; j++) {
+                if (hand[i] == hand[j]) {
+                    for (int k = j + 1;k < HANDSIZE; k++) {
+                        if (hand[j] == hand[k]) {
+                            if (k < HANDSIZE - 1) {
+                                return (hand[k] == hand[k + 1]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public static boolean containsFullHouse(int[] hand) {
-        boolean result = false;
-        return result;
+        int pairs = 0;
+        for (int i = 0; i < HANDSIZE; i++) {
+            for (int j = i + 1; j < HANDSIZE; j++) {
+                if (hand[i] == hand[j]) {
+                    pairs++;
+                }
+            }
+        }
+        return (pairs == 4);
     }
 
     public static boolean containsStraight(int[] hand) {
-        boolean result = false;
-        return result;
+        for (int i = 0; i < HANDSIZE; i++) {
+            if (i < HANDSIZE - 1) {
+                if (hand[i] != (hand[i + 1] - 1)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static boolean containsThreeOfaKind(int[] hand) {
-        boolean result = false;
-        return result;
+        for (int i = 0; i < HANDSIZE; i++) {
+            for (int j = i + 1; j < HANDSIZE; j++) {
+                if (hand[i] == hand[j]) {
+                    for (int k = j + 1;k < HANDSIZE; k++) {
+                        if (hand[j] == hand[k]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public static boolean containsTwoPair(int[] hand) {
-        boolean result = false;
-        return result;
+        int pairs = 0;
+        for (int i = 0; i < HANDSIZE; i++) {
+            for (int j = i + 1; j < HANDSIZE; j++) {
+                if (hand[i] == hand[j]) {
+                    pairs++;
+                }
+            }
+        }
+        return (pairs == 2);
     }
 
     public static boolean containsPair(int[] hand) {
-        boolean result = false;
-        return result;
+        for (int i = 0; i < HANDSIZE; i++) {
+            for (int j = i + 1; j < HANDSIZE; j++) {
+                if (hand[i] == hand[j]) {
+                     return true;
+                }
+            }
+        }
+        return false;
     }
 }
